114eb135b341df5da88b4d49998276a4996a0330
Ivan Palii <vanopaliy@gmail.com>
2023-06-29 04:18:49 +0900

    first task

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
 "use strict";
 
-console.log("Hello world")
 
-console.log("Second line")
+/*
+Вкажіть принаймні один спосіб доступу до кожного з перелічених нижче DOM вузлів:
 
-console.log("Third line")
+До DOM вузла <div>?
+*/
+const divELem = document.body.firstElementChild
+console.log(divELem)
+/*
+До DOM вузла <ul>?
+ */
+const ulElem = divELem.nextElementSibling
+console.log(ulElem)
+/*
+До другого <li> (Петро)?*/
+const lastLiElem = ulElem.lastElementChild
+console.log(lastLiElem)
+
+
+
+
+
+
