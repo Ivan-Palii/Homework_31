e5d8b04e3c536e536e59f23505d55821e92650a5
Ivan Palii <vanopaliy@gmail.com>
2023-06-29 05:25:00 +0900

    third task

--- js/script.js
+++ js/script.js
@@ -1,25 +1,30 @@
 "use strict";
 
-
 /*
-Вкажіть принаймні один спосіб доступу до кожного з перелічених нижче DOM вузлів:
-
-До DOM вузла <div>?
+* Як знайти?…
 */
-const divELem = document.body.firstElementChild
-console.log(divELem)
-/*
-До DOM вузла <ul>?
- */
-const ulElem = divELem.nextElementSibling
-console.log(ulElem)
-/*
-До другого <li> (Петро)?*/
-const lastLiElem = ulElem.lastElementChild
-console.log(lastLiElem)
 
+//Таблиця з id="age-table".
+const table = document.getElementById('age-table')
+console.log(table)
 
+//Усі елементи label всередині цієї таблиці (їх має бути 3).
+const labels = table.getElementsByTagName('label')
+console.log(labels)
 
+//Перший td у цій таблиці (зі словом “Age”).
+const firstTd = table.rows[0].cells[0]
+console.log(firstTd)
 
+//form з name="search".
+const form = document.querySelector('form[name="search"]')
+console.log(form)
 
+const formInputs = form.getElementsByTagName('input')
+//Перший input у цій формі.
+const firstInp  = formInputs[0]
+console.log(firstInp)
 
+//Останній input у цій формі.
+const lastInp = formInputs[formInputs.length-1]
+console.log(lastInp)
